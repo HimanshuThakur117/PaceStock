4c35339ba52799c9d518029e5d34b361cf47c2ee
HimanshuThakur117 <himanshu.singh.work@gmail.com>
2022-08-17 02:05:54 +0530

    api integrate , custom widget maked

--- lib/color.dart
+++ lib/color.dart
@@ -4,6 +4,7 @@ class AppColor {
 
   static Color headerColor = const Color(0xff000000);
   static Color backgroundColor = const Color(0xff464646);
-  static Color textColor = const Color(0xffFFFFFF);
+  static Color textColor = const Color(0xffF2F2F2);
+  static Color headingColor = const Color(0xffFFFFFF);
 
 }
--- lib/home.dart
+++ lib/home.dart
@@ -6,6 +6,9 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pace_stock/color.dart';
 import 'package:http/http.dart' as http;
+import 'package:pace_stock/containerWidget.dart';
+
+import 'fetchApi.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,31 +18,14 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 
-  List Images = [];
+
+late Future<List> news;
 
   @override
   void initState() {
     super.initState();
-    fetchApi();
+    news = fetchnews();
   }
-
-  String Apikey = '4b0b6c2daae747a1baa877c1bffae818';
-
-  fetchApi() async {
-    await http.get(Uri.parse("https://newsapi.org/v2/top-headlines?country=in"),
-        headers: {
-          'Authorization': '4b0b6c2daae747a1baa877c1bffae818'
-        }).then((value) {
-      print(value.body);
-     
-      // setState(() {
-      //   Images= result['urlToImage'];
-      // });
-      //  print(Images);
-    });
-   
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,25 +38,42 @@ class _HomeScreenState extends State<HomeScreen> {
           style: GoogleFonts.robotoSlab(
             fontSize: 29,
             fontWeight: FontWeight.bold,
-            color: AppColor.textColor,
+            color: AppColor.headingColor,
           ),
         ),
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              child: Column(
-                children: [
-                  Text(
-                    "HEADLINES",
-                    style: GoogleFonts.robotoSlab(
-                      fontSize: 29,
-                      fontWeight: FontWeight.bold,
-                      color: AppColor.textColor,
-                    ),
-                  ),
-                ],
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Container(
+                height: 300,
+                width: MediaQuery.of(context).size.width,
+                child: FutureBuilder(
+                  future:  fetchnews(),
+                  builder: ((context, snapshot) {
+
+                    if(snapshot.hasData) {
+
+                       return ListView.builder(
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: ((context, index) {
+                          return NewBox(
+
+                          );
+                        })
+                        );    
+
+                    } else if(snapshot.hasError){
+                      return Text("${snapshot.error}");
+                    }
+                    return Center(
+                          child: CircularProgressIndicator(
+                        color: AppColor.headingColor,
+                      ));
+                })
+                ),
               ),
             ),
           ],
